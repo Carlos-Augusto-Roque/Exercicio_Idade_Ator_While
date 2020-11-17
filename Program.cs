a9a6774e81bdb4613f6c842ec52466461f948430
carlos <carlosacgr@gmail.com>
2020-11-17 19:00:54 -0300

    Exercício Idade do Ator (While e DoWhile)

--- Program.cs
+++ Program.cs
@@ -6,16 +6,26 @@ namespace Exercicio_Idade_Ator_While
     {
         static void Main(string[] args)
         {
-            Console.Write("Digite a idade do ator: ");
-            int idade = int.Parse(Console.ReadLine());
+            Console.Write("Digite a idade do ator: ");//exibição da mensagem
+            int idade = int.Parse(Console.ReadLine());//entrada e armazenamento da idade
 
-            while(idade != 47)
+            while(idade != 47)//se a idade digitada for diferente de 47,então :
             {
-                Console.WriteLine("Errou !");
-                Console.WriteLine("Tente novamente");
-                idade = int.Parse(Console.ReadLine());
+                Console.WriteLine("Errou !");//exibir mensagem que errou
+                Console.WriteLine("Tente novamente");//exibir a mensagem para tentar novamente
+                idade = int.Parse(Console.ReadLine());//entrada e armazenamento da idade 
             }
-            Console.WriteLine("Resposta Correta -- Parabéns !");
+            Console.WriteLine("Resposta Correta -- Parabéns !");//se  a idade digitada for 47,então exibir a mensagem que está correta
+
+            
+            //mesmo exemplo,agora usando o (do while)
+            // int idade;
+
+            // do{
+            //     Console.WriteLine("digite a idade do ator");
+            //     idade = int.Parse(Console.ReadLine());
+            // }while(idade != 47);
+            // Console.WriteLine("Acertou");
         }
     }
 }
