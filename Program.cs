d6e17ea3b1c5b3d03c25785a74a3867daaabd4e2
carlos <carlosacgr@gmail.com>
2020-11-17 18:51:40 -0300

    Exercício idade do ator - While

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio_Idade_Ator_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a idade do ator: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            while(idade != 47)
+            {
+                Console.WriteLine("Errou !");
+                Console.WriteLine("Tente novamente");
+                idade = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Resposta Correta -- Parabéns !");
+        }
+    }
+}
